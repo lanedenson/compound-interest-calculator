7df6a80fc1b0383c51352648a5829c1189aa5ee5
Lane Denson <lanedenson@gmail.com>
2024-11-24 18:15:08 -0600

    Enhance user input handling and output formatting
    - Updated input prompts to allow for decimal values for principal, interest rate, and time.
    - Implemented formatting for output values to display commas as thousands separators.
    - Added logic to conditionally display decimals based on user input.

--- interest.py
+++ interest.py
@@ -5,7 +5,7 @@ while True:
     print("\n")
     #First, let's gather the information about the investment.
     principal = float(input("What is the initial investment (or \"principal\") amount? "))
-    print(f"You entered: ${principal}.\n")
+    print(f"You entered: ${principal:,.2f}\n")
 
     interest_rate = float(input("What is the interest rate? ")) / 100
     print(f"You entered: {100 * interest_rate}% interest rate.\n")
@@ -29,16 +29,14 @@ while True:
     print(f"The growth factor per compounding period is the periodic interest rate ({periodic_interest_rate}) plus 1, or {growth_factor}.\n")
 
     compound_growth_factor = growth_factor ** times_compounded
-
     print(f"The compound growth factor is the growth factor {growth_factor} raised to the power of {times_compounded}, the times compounded, for a total of {compound_growth_factor}.\n")
 
     future_value = round(principal * compound_growth_factor, 2)
-
-    print(f"Finally, we calculate the total future value (including the initial investment) by multiplying the principal of ${principal} by the compound growth factor of {compound_growth_factor}.\n")
-
-    print(f"...for a total of ${future_value}.\n")
+    print(f"Finally, we calculate the total future value (including the initial investment) by multiplying the principal of ${principal:,.2f} by the compound growth factor of {compound_growth_factor}.\n")
+    print(f"...for a total of ${future_value:,.2f}.\n")
 
     try_again = input("Would you like to try again (y to try again, any other key to exit)?")
 
     if try_again != "y":
+        print("\nGoodbye!\n")
         break
