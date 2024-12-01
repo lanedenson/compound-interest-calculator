1225be318f84b0c72f3cd4e7167cb63b69837353
Lane Denson <lanedenson@gmail.com>
2024-11-30 21:22:40 -0600

    Added helper function to the simpler calculator

--- interest-less-detail.py
+++ interest-less-detail.py
@@ -2,52 +2,46 @@ def future_value(principal: float, interest_rate: float, compounded: int, time: 
     '''Calculate the future value based on inputs. Set float and int datatypes accordingly.'''
     return principal * ((1 + ((interest_rate / 100) / compounded)) ** (compounded * time))
 
-def get_positive_float(prompt):
-    '''Prompt for floating values (e.g. principal and interest rate)
-    
-    Return error if value is less than 0 or if invalid characters are entered
-    
-    Strip out anticipated characters (&,%)
+#Define the input functions
+def get_positive_value(prompt: str, value_type: type, strip_chars: list = None):
+    '''Validating input: checking for numerals only, removing empty trailing or leading characters, stripping out anticipated characters
     '''
     while True:
-        value = input(prompt)
-        value = value.replace(',', '').replace('$', '').replace('%', '')
+        value = input(prompt).strip()
+        if strip_chars:
+                for char in strip_chars:
+                    value = value.replace(char, '')
+        if not value:
+            print("Please enter a value.")
+            continue
         try:
-            value = float(value)
+            value = value_type(value)
             if value < 0:
                 print("Please enter a number greater than 0.")
             else:
                 return value
         except ValueError:
             print("Please enter a valid number.")
-            
-def get_positive_int(prompt):
-    '''Prompt for integer values (e.g. times compounded and length of investment)
-    
-    Return error if value is less than 0 or if invalid characters are entered
-    '''
-    while True:
-        value = input(prompt)
-        try:
-            value = int(value)
-            if value < 0:
-                print("Please enter a number greater than 0.")
-            else:
-                return value
-        except ValueError:
-            print("Please enter a valid number.")
+
+def get_float(prompt: str):
+    return get_positive_value(prompt, float, strip_chars=[',', '$', '%'])
+
+def get_integer(prompt: str):
+    return get_positive_value(prompt, int)
+
+
 
 print("\nCompound Interest Calculator")
 
 while True:
     print("\n")
-    principal = get_positive_float("What is the initial investment (or \"principal\") amount? ")
+    principal = get_float("What is the initial investment (or \"principal\") amount? ")
 
-    interest_rate = get_positive_float("What is the interest rate? ")
+    interest_rate = get_float("What is the interest rate? ")
 
-    compounded = get_positive_int("How often will this be compounded in a year? ")
+    compounded = get_integer("How often will this be compounded in a year? ")
 
-    time = get_positive_int("How long will this be invested (in years)? ")
+    time = get_integer("How long will this be invested (in years)? ")
 
     print(f"The total future value of the investment is ${future_value(principal, interest_rate, compounded, time):,.2f}.\n")
 
